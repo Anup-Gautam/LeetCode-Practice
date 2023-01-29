90b4a3bf298bba48f4cb85ef5dd3a37ddc2269cf
Anup-Gautam <84380379+Anup-Gautam@users.noreply.github.com>
2023-01-29 17:18:58 -0600

    Update Contains_Duplicate.py

--- Array and Hashing/Contains_Duplicate.py
+++ Array and Hashing/Contains_Duplicate.py
@@ -8,3 +8,8 @@ class Solution:
             else:
                 return True # since the char is already in the Dictonary, it denotes that duplicate is present
         return False
+    
+    #Time Complexity = o(n)
+    #Space Complexity = o(n)
+    
+   #in order to do it space complexity o(1), we can use sort but it will change the time complexity to o(n^2)
