5053c39f81283d79379b38b1a954a0f7b359cfb6
Anup-Gautam <84380379+Anup-Gautam@users.noreply.github.com>
2023-01-31 00:07:52 -0600

    Create Valid_anagram.py

---
+++ Array and Hashing/Valid_anagram.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # if the length is not same they cannot be anagrams
+        if len(s) != len(t):
+            return False
+        #create a dictionary to count char in each string
+        countS = {}
+        countT = {}
+        for idx in range(len(s)):
+            countS[s[idx]] = 1 + countS.get(s[idx],0) #creates a counter for each letter
+            countT[t[idx]] = 1 + countT.get(t[idx],0)
+        if countS != countT: # checking if dictionary has same keys and values
+                return False
+        return True
