8aeb91c68028e2f2dda3f944c4620246c3ce2109
Anup-Gautam <84380379+Anup-Gautam@users.noreply.github.com>
2023-02-07 22:53:26 -0600

    Update Product_of_Array_Except_Self.py

--- Array and Hashing/Product_of_Array_Except_Self.py
+++ Array and Hashing/Product_of_Array_Except_Self.py
@@ -1,13 +1,22 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = [1] * (len(nums))
+           # output List
+            res = [1] * len(nums)
+            # determining a multiplier prefix
+            prefix = 1
+            for idx in range(len(nums)):
+                res[idx] = prefix 
+                prefix *= nums[idx]# multiplies and stores the prefix in output List
+            #determining a multiplier postfix
+            postfix = 1
+            for idx in range(len(nums)-1,-1,-1):
+                res[idx] *= postfix # not equating because it will overwrite res value
+                postfix *= nums[idx] 
+            
+            return res
 
-        prefix = 1
-        for i in range(len(nums)):
-            res[i] = prefix
-            prefix *= nums[i]
-        postfix = 1
-        for i in range(len(nums) - 1, -1, -1):
-            res[i] *= postfix
-            postfix *= nums[i]
-        return res
+            
+            
+            
+            #time complexity = O(n)
+            #space complexity = O(1)
